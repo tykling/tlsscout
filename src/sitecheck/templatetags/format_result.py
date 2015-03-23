f5fb48d869bcfaed7a7394ec6234a40ab5fc3ad7
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-03-23 23:20:19 +0100

    work on formatting results

---
+++ src/sitecheck/templatetags/format_result.py
@@ -0,0 +1,34 @@
+from django import template
+from collections import Iterable
+register = template.Library()
+
+@register.filter
+def format_result(value):
+    if not isinstance(value, Iterable):
+        value = [value]
+    output = ""
+    for result in value:
+        if result.grade == "A+":
+            labelclass="label-success"
+        elif result.grade == "A":
+            labelclass="label-success"
+        elif result.grade == "A-":
+            labelclass="label-success"
+        elif result.grade == "B":
+            labelclass="label-warning"
+        elif result.grade == "C":
+            labelclass="label-warning"
+        elif result.grade == "D":
+            labelclass="label-danger"
+        elif result.grade == "E":
+            labelclass="label-danger"
+        elif result.grade == "F":
+            labelclass="label-danger"
+        else:
+            labelclass="label-default"
+            result.grade = "<i class='fa fa-frown-o fa-lg'></i>"
+            if result.status_message and result.status_message != "":
+                output += "<div style='font-size: 36px'><span data-content='%s' data-placement='right' data-original-title='Error Message' data-trigger='hover click' class='popoverlabel label %s'>%s</span></div>" % (result.status_message, labelclass, result.grade)
+        output += "<div style='font-size: 36px'><span class='label %s'>%s</span></div>" % (labelclass, result.grade)
+    return output
+
