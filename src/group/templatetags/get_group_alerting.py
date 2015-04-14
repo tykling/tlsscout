5b7ba4843654b61aa72672464a9769411c118caf
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-04-15 00:32:19 +0200

    use user.id to force instantiation of lazy user object

--- src/group/templatetags/get_group_alerting.py
+++ src/group/templatetags/get_group_alerting.py
@@ -5,7 +5,7 @@ from alert.models import GroupAlert
 @register.filter
 def get_group_alerting(group, user):
     try:
-        groupalert = GroupAlert.objects.get(group=group, user=user)
+        groupalert = GroupAlert.objects.get(group=group, user=user.id)
         return groupalert.id
     except GroupAlert.DoesNotExist:
         return False
