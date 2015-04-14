f3313a98e2316e6e8f8449d47d070746b4d73ed8
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-04-14 20:13:42 +0200

    remove debug print

--- src/sitecheck/templatetags/relative_date_hours.py
+++ src/sitecheck/templatetags/relative_date_hours.py
@@ -5,5 +5,4 @@ register = template.Library()
 @register.filter(expects_localtime=True)
 def relative_date_hours(value, arg):
     """add or substract a given number of hours from a datetime"""
-    print "adding %s to timedelta" % value
     return value + timedelta(hours=arg)
