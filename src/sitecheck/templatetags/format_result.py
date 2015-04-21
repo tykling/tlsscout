16a4fddc4f2f370b713fb2dfe38dda3ea3a53d4a
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-04-21 16:14:39 +0200

    more result formatting

--- src/sitecheck/templatetags/format_result.py
+++ src/sitecheck/templatetags/format_result.py
@@ -11,61 +11,77 @@ def format_result(value):
     for result in value:
         if result.grade == "A+":
             formatdict = {
-                'gradenumber': 8,
+                'gradenumber': 9,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-success',
                 'grade': result.grade,
             }
         elif result.grade == "A":
             formatdict = {
-                'gradenumber': 7,
+                'gradenumber': 8,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-success',
                 'grade': result.grade,
             }
         elif result.grade == "A-":
             formatdict = {
-                'gradenumber': 6,
+                'gradenumber': 7,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-success',
                 'grade': result.grade,
             }
         elif result.grade == "B":
             formatdict = {
-                'gradenumber': 5,
+                'gradenumber': 6,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-warning',
                 'grade': result.grade,
             }
         elif result.grade == "C":
             formatdict = {
-                'gradenumber': 4,
+                'gradenumber': 5,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-warning',
                 'grade': result.grade,
             }
         elif result.grade == "D":
             formatdict = {
-                'gradenumber': 3,
+                'gradenumber': 4,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-danger',
                 'grade': result.grade,
             }
         elif result.grade == "E":
             formatdict = {
-                'gradenumber': 2,
+                'gradenumber': 3,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-danger',
                 'grade': result.grade,
             }
         elif result.grade == "F":
             formatdict = {
-                'gradenumber': 1,
+                'gradenumber': 2,
                 'headline': 'Messages for %s' % result.serverip,
                 'labelclass': 'label-danger',
                 'grade': result.grade,
             }
-        else:
+        elif result.grade == "M":
+            formatdict = {
+                'gradenumber': 1,
+                'headline': 'Messages for %s' % result.serverip,
+                'labelclass': 'label-danger',
+                'grade': result.grade,
+                'messages': 'Certificate name mismatch',
+            }
+        elif result.grade == "T":
+            formatdict = {
+                'gradenumber': 1,
+                'headline': 'Messages for %s' % result.serverip,
+                'labelclass': 'label-danger',
+                'grade': result.grade,
+                'messages': 'Certificate not trusted',
+            }
+        elif result.grade is None:
             if result.status_message and result.status_message != "":
                 formatdict = {
                     'gradenumber': 0,
@@ -74,12 +90,13 @@ def format_result(value):
                     'grade': "X",
                     'messages': result.status_message,
                 }
-            else:
-                ### no results or messages yet
-                continue
+        else:
+            ### no results or messages yet
+            continue
 
         if 'messages' not in formatdict:
             formatdict['messages'] = "No messages"
+
         output += render_to_string('includes/format_result.html', formatdict)
     return output
 
