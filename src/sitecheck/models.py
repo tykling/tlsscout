608c1e82a016a8c1eb2767fbce71a3083a76af6e
Thomas Steen Rasmussen <thomas@gibfest.dk>
2018-03-13 17:07:23 +0100

    make a few fields blank=True and make the sleeping until message a bit shorter

--- src/sitecheck/models.py
+++ src/sitecheck/models.py
@@ -3,11 +3,11 @@ from django.db import models
 class SiteCheck(models.Model):
     site = models.ForeignKey('tlssite.Site', related_name='checks', on_delete=models.PROTECT)
     urgent = models.BooleanField(default=False)
-    start_time = models.DateTimeField(null=True)
-    status = models.TextField(null=True)
-    finish_time = models.DateTimeField(null=True)
-    json_result = models.TextField(null=True)
-    status_message = models.TextField(null=True)
+    start_time = models.DateTimeField(null=True, blank=True)
+    status = models.TextField(null=True, blank=True)
+    finish_time = models.DateTimeField(null=True, blank=True)
+    json_result = models.TextField(null=True, blank=True)
+    status_message = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return "#%s %s: %s - %s (%s)" % (self.id, self.site.hostname, self.start_time, self.finish_time, self.status)
