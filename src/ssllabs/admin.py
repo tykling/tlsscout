847cef139e65f491721893fcea98ee313a0d9e0b
Thomas Steen Rasmussen <thomas@gibfest.dk>
2018-03-13 13:28:50 +0100

    make sitecheck readonly so admin doesn't have to render a huge dropdown

--- src/ssllabs/admin.py
+++ src/ssllabs/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.ApiClientState)
-admin.site.register(models.RequestLog)
+
+class RequestLogAdmin(admin.ModelAdmin):
+    readonly_fields = ['sitecheck']
+
+admin.site.register(models.RequestLog, RequestLogAdmin)
 
